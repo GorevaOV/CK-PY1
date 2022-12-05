d5fe988b489417355a95596ac97d78dc3e09c302
GorevaOV <119260614+GorevaOV@users.noreply.github.com>
2022-12-05 14:35:40 +0300

    Update task_9.py

--- Лабораторная работа 3/task_9.py
+++ Лабораторная работа 3/task_9.py
@@ -5,9 +5,10 @@ increase = 0.03  # рост цен
 money_capital = 0  # количество денег, чтобы прожить 10 месяцев
 delta = 0 # разница расходов и доходов
 
-for i in range(months): # запускаем конечное количество циклов
+for _ in range(months): # запускаем конечное количество циклов
     delta = spend - salary # формула переменной
     spend *= 1 + increase # увеличение расходов
     money_capital += delta # просчет необходимой фин. подушки
 
 print(round(money_capital)) # финал
+# Последняя строка
